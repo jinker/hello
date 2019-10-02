a37650d05ade071f4daabfa0a07d5ef3fbe7e002
蒋津 <jiangj01@mysoft.com.cn>
2019-10-02 21:36:31 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jinker/hello
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "github.com/user/stringutl"
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+}
